8e13a5595c4cb61ccffc13dc3b793fd29c5caa79
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-11-25 14:29:57 +0200

    Added Toast Provider

--- src/pages/task-page/TasksPage.tsx
+++ src/pages/task-page/TasksPage.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { Box } from "@mui/material";
 
-import AddTaskModal, { type AddTaskFormValues } from "../../components/modal/AddTaskModal";
+import AddTaskModal from "../../components/modal/AddTaskModal";
 import TaskTable from "../../components/task-table/TaskTable";
+import AppHeader from "../../components/header/AppHeader";
 
 import { initialTasks } from "../../mockData/mock-data";
-import AppHeader from "../../components/header/AppHeader";
+import type { AddTaskFormValues } from "../../components/modal/Modal.types";
+import { useToast } from "../../providers/ToastProvider";
 
 export default function TasksPage() {
   const [open, setOpen] = useState(false);
   const [tasks, setTasks] = useState<any[]>(initialTasks);
+  const { showToast } = useToast();
 
   const handleAddTask = (task: AddTaskFormValues) => {
     setTasks((prev) => [
@@ -24,6 +27,8 @@ export default function TasksPage() {
         status: "In progress"
       }
     ]);
+
+    showToast("Task added successfully", "success");
   };
 
   return (
